9e3e5f83b1644165be7bed4cecbca3676b02f041
Manish Rana <alertmanishrana@gmail.com>
2019-09-22 20:00:34 +0545

    kdn

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="helloReact">
+//       Hello React!
+//     </div>
+//   );
+// }
+
+const ChildComponent = ()=>{
+	return(
+		<div>
+			<h5 className="childComponent">I am a Child Compnent</h5>
+		</div>
+		);
+	};
+
+
+
+const TypesOfFruits = ()=>{
+	return(
+		<ul>
+			<li>Mango</li>
+			<li>Banana</li>
+			<li>Apple</li>
+			<li>Orange</li>
+			<li>Pomegranate</li>
+			<li>WildOclose</li>
+		</ul>
+	);
 }
 
-export default App;
+const Fruits = ()=>{
+	return(
+		<div>
+			<h3>Types Of Fruits: </h3>
+			<TypesOfFruits />
+		</div>
+		);
+};
+
+class TypesOfFoods extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<div>
+				<h1>Types Of Food:</h1>
+				<Fruits/>
+			</div>
+			);
+	}
+}
+
+class ParentComponent extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<div>
+				<h1 className="parentComponent">I am the Parent Element</h1>
+				<ChildComponent/>	
+			</div>
+			);
+	}
+};
+
+
+// export default App;
+export default ParentComponent;
