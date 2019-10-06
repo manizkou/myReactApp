74b0976913caec04c9ed509dd491457524192db3
Manish Rana <alertmanishrana@gmail.com>
2019-10-06 14:15:26 +0545

    react

--- src/App.js
+++ src/App.js
@@ -20,28 +20,69 @@ const ChildComponent = ()=>{
 		);
 	};
 
-
-
-const TypesOfFruits = ()=>{
+const NonCitrus = ()=>{
 	return(
+		<div>
+		<h2>Non-Citrus:</h2>
 		<ul>
-			<li>Mango</li>
-			<li>Banana</li>
-			<li>Apple</li>
-			<li>Orange</li>
-			<li>Pomegranate</li>
-			<li>WildOclose</li>
+			<li>Apples</li>
+			<li>Blueberries</li>
+			<li>Strawberries</li>
+			<li>Bananas</li>
 		</ul>
+		</div>
 	);
-}
+};
+
+const Citrus = ()=>{
+	return(
+		<div>
+		<h2>Citrus:</h2>
+		<ul>
+			<li>Lemon</li>
+			<li>Lime</li>
+			<li>Orange</li>
+			<li>GrapeFruit</li>
+		</ul>
+		</div>
+	);
+};
+
+
+// const TypesOfFruits = ()=>{
+// 	return(
+// 		<ul>
+// 			<li>Mango</li>
+// 			<li>Banana</li>
+// 			<li>Apple</li>
+// 			<li>Orange</li>
+// 			<li>Pomegranate</li>
+// 			<li>WildOclose</li>
+// 		</ul>
+// 	);
+// }
 
 const Fruits = ()=>{
 	return(
 		<div>
-			<h3>Types Of Fruits: </h3>
-			<TypesOfFruits />
+			<h1>Fruits: </h1>
+			<NonCitrus/>
+			<Citrus/>
 		</div>
 		);
+};
+
+const Vegetables = ()=>{
+	return(
+		<div>
+			<h1>Vegetables:</h1>
+			<ul>
+				<li>Brussel Sprouts</li>
+				<li>Broccoli</li>
+				<li>Squash</li>
+			</ul>
+		</div>
+	);
 };
 
 class TypesOfFoods extends React.Component{
@@ -52,13 +93,61 @@ class TypesOfFoods extends React.Component{
 	render(){
 		return(
 			<div>
-				<h1>Types Of Food:</h1>
-				<Fruits/>
+				<h2>Types Of Food:</h2>
+				<Fruits />
+				<Vegetables />
 			</div>
 			);
 	}
+};
+
+const CurrentDate = (props)=>{
+	return(
+		<h2>The current date is: {props.date}</h2>
+		// <h2>{props.date}</h2>
+
+	);
 }
 
+class Calender extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<CurrentDate date={Date()} />
+		);
+	}
+}
+
+const List = (props)=>{
+	return(
+		<p>{props.tasks.join(", ")}</p>
+	);
+}
+
+
+class ToDo extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<div>
+			<h4>
+				Today
+			</h4>
+			<List tasks={["Javascript", "React"]}/>
+			<h4>
+				Tomorrow
+			</h4>
+			<List tasks={["CodeIgnitor","Ajax"]}/>
+			</div>
+		);
+	}
+}
 class ParentComponent extends React.Component{
 	constructor(props){
 		super(props);
@@ -67,8 +156,11 @@ class ParentComponent extends React.Component{
 	render(){
 		return(
 			<div>
+				<Calender/>
 				<h1 className="parentComponent">I am the Parent Element</h1>
-				<ChildComponent/>	
+				<ChildComponent/>
+				<ToDo />
+				<TypesOfFoods />	
 			</div>
 			);
 	}
