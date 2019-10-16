0fe89c95e0c1ef28d4bd43e35d6195bca62995d3
Manish Rana <alertmanishrana@gmail.com>
2019-10-16 16:54:48 +0545

    stateful

--- src/StatefulComponent.js
+++ src/StatefulComponent.js
@@ -7,14 +7,19 @@ class StatefulComponent extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			name: "Manish"
+			name : "StatefulComponent",
+			property : "I have states"
 		};
 	}
 
 	render(){
+
+		const property = this.state.property;
+		
 		return(
 			<div>
-				<h1>Hello there {this.state.name}! </h1>
+				<h1>Hello there this is {this.state.name}! </h1>
+				<h4>Property: {property}</h4>
 			</div>
 		);
 	}
